e5796f39bdad40b06daf1f2a38ffafb76f7ccddf
Arcane52689 <tjenkins52689@gmail.com>
2015-11-18 11:52:28 -0500

    add metaprogramming to deck

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,9 @@ class SessionsController < ApplicationController
 
 
   def create
+    byebug
     user = User.find_by_credentials(
-    params[:user][:email_or_password],
+    params[:user][:email_or_username],
     params[:user][:password]
     )
     login(user)
@@ -16,6 +17,7 @@ class SessionsController < ApplicationController
 
   def destroy
     current_session = Session.find_by(token: session[:session_token])
+    session[:session_token] = nil
     current_session.destroy!
     redirect_to new_session_url
   end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,49 +1,68 @@
 class Deck < ActiveRecord::Base
   attr_accessor :card_limit, :card_minimum
+  @format = "Deck"
+  @card_minimum = 0
+  @card_limit = Float::INFINITY
+  validates :name, presence: true
+
   belongs_to :user
   # validate :not_too_many_cards, :not_too_few_cards
   after_initialize :set_variables
 
+  def self.all_in_format
+    self.all.where(format: @format)
+  end
+
+  def self.format
+    @format
+  end
+
+  def self.card_limit
+    @card_limit
+  end
+
+  def self.card_minimum
+    @card_minimum
+  end
 
 
   def set_variables
-    self.card_limit = Float::infinity
-    self.card_minimum = 0
-    self.format = 'Deck'
+    self.card_limit = self.class.card_limit
+    self.card_minimum = self.class.card_minimum
+    self.format ||= self.class.format
   end
 
 end
 
 
 class Commander < Deck
-  def set_variables
-    self.card_limit = 100
-    self.card_minimum = 100
-    self.format = 'Commander'
-  end
+  @format = "Commander"
+  @card_limit = 100
+  @card_maximum = 100
+
+
+
+
+
 
 end
 
 class TinyLeaders < Deck
-  def set_variables
-    self.card_limit = 50
-    self.card_minimum = 50
-    self.format = 'Tiny Leaders'
-  end
+  @format = "Tiny Leaders"
+  @card_limit = 50
+  @card_maximum = 50
+
+
 end
 
 class Standard < Deck
-  def set_variables
-    self.card_limit = Float::INFINITY
-    self.card_minimum = 60
-    self.format = 'Standard'
-  end
+  @format = "Standard"
+  @card_limit = 60
+
+
 end
 
 class Modern < Deck
-  def set_variables
-    self.card_limit = Float::INFINITY
-    self.card_minimum = 60
-    self.format = 'Standard'
-  end
+  @format = "Modern"
+  @card_limit = 60
 end
