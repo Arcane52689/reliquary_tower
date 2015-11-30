2ffab203db2ec6c390bc627475eefed6555212da
Arcane52689 <tjenkins52689@gmail.com>
2015-11-30 13:57:55 -0500

    Begin work onn user login from angular

--- app/assets/javascripts/controllers/page_select_controller.js
+++ app/assets/javascripts/controllers/page_select_controller.js
@@ -16,6 +16,32 @@ angular.module('AppControllers').controller('PageSelectCtrl', ['Selected', funct
     }
   }
 
+  this.goToPage = function(page) {
+    if ((page > 0) && (page < this.pageInfo.pages)) {
+      this.pageInfo.currentPage = page
+    }
+  }
+
+  this.pageList = function() {
+    var page
+    var pages = [];
+    for ( var i = -2; i <3; i++) {
+      page = this.pageInfo.currentPage + i;
+      if ((page > 1) && (page < this.pageInfo.pages)) {
+        pages.push(page);
+      }
+    }
+    return pages
+  }
+
+  this.isSelected = function(page) {
+    if (page === this.pageInfo.currentPage) {
+      return "selected";
+    } else {
+      return "";
+    }
+  }
+
 
 
   this.initialize();
